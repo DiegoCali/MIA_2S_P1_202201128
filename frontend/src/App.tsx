a8dc48bc47081ffd955e05ed816cb67107ffce21
Diego Cali <diegocali123@gmail.com>
2024-07-31 18:14:26 -0600

    Added code editor and buttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Editor from "@monaco-editor/react";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          This is for testing!!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <h2>[MIA] Proyecto 1 Diego Cali</h2>
+          <Editor
+              height="45vh"
+              width="180vh"
+              theme="vs-dark"
+              loading="Loading..."
+              defaultLanguage="javascript"
+              defaultValue="// some comment"
+          />
+          <Stack spacing={2} direction="row">
+              <Button variant="contained">File</Button>
+              <Button variant="contained">Run</Button>
+          </Stack>
+      </>
   )
 }
 
