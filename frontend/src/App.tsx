57f2afb49f3afb06571a650505e46f260b5ccc2e
Diego Cali <diegocali123@gmail.com>
2024-07-31 21:48:47 -0600

    feature: Console Log. To Do: File Selector and Endpoints to Backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,55 @@
 import Editor from "@monaco-editor/react";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
-import Textarea from "@mui/joy/Textarea";
+import CssBaseline from "@mui/material/CssBaseline";
+import {createTheme, ThemeProvider} from "@mui/material";
+import {useRef} from "react";
 import './App.css'
 
-function App() {
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
 
+function App() {
+    const editorRef = useRef(null);
+
+    function handleEditorDidMount(editor: any){
+        editorRef.current = editor;
+    }
+
+    function addText(){
+        document.getElementById("Console").innerText = editorRef.current.getValue()
+    }
   return (
+
       <>
-          <h2>[MIA] Proyecto 1 Diego Cali</h2>
-          <Editor
-              height="45vh"
-              width="180vh"
-              theme="vs-dark"
-              loading="Loading..."
-              defaultLanguage="javascript"
-              defaultValue="// some comment"
-          />
-          <Stack spacing={2} direction="row">
-              <Button variant="contained">File</Button>
-              <Button variant="contained">Run</Button>
-          </Stack>
-          <Textarea></Textarea>
+          <ThemeProvider theme={darkTheme}>
+              <CssBaseline />
+              <h2>[MIA] Proyecto 1 Diego Cali</h2>
+              <Editor
+                  height="45vh"
+                  width="180vh"
+                  theme="vs-dark"
+                  loading="Loading..."
+                  defaultLanguage="javascript"
+                  defaultValue="// some comment"
+                  onMount={handleEditorDidMount}
+              />
+              <Stack spacing={2} direction="row">
+                  <Button variant="outlined">File</Button>
+                  <Button variant="outlined" onClick={addText}>Run</Button>
+              </Stack>
+              <textarea
+                  id="Console"
+                  style={{
+                      width: "100%",
+                      height: "100%",
+                  }}
+                  rows={5}
+              ></textarea>
+          </ThemeProvider>
       </>
   )
 }
